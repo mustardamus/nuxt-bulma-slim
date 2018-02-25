b1ce655cae4cd481a40d69a61f923e6aaaba5b87
Sebastian Senf <contact@mustardamus.com>
2018-02-25 12:49:11 +0100

    make the main test work again

--- test/index.test.js
+++ test/index.test.js
@@ -39,8 +39,8 @@ describe('Nuxt Module', () => {
         build: {}
       }
     }
-    const sassTempPath = join(tmpdir(), 'nuxt-bulma-slim.full.sass')
-    const options = { sassTempPath, variablesPath: false }
+    const sassTempPath = join(tmpdir(), 'nuxt-bulma-slim.sass')
+    const options = { sassTempPath, variablesPath: '/nope' }
 
     nuxtModule.call(context, options)
 
@@ -50,11 +50,11 @@ describe('Nuxt Module', () => {
 
     const lines = readFileSync(sassTempPath, 'utf8').split('\n')
 
-    expect(lines).toHaveLength(6) // _all
-    expect(lines[0].includes(bulmaPath)).toBe(true)
+    expect(lines).toHaveLength(1)
+    expect(lines[0]).toBe(`@import "${join(bulmaPath, 'bulma.sass')}"`)
   })
 
-  it('should write the slim bulma sass build if on production', () => {
+  it('should write the slim bulma css build if on production', () => {
     const context = {
       options: {
         dev: false,
@@ -64,19 +64,14 @@ describe('Nuxt Module', () => {
         build: {}
       }
     }
-    const sassTempPath = join(tmpdir(), 'nuxt-bulma-slim.slim.sass')
-    const options = { sassTempPath, variablesPath: false }
+    const cssTempPath = join(tmpdir(), 'nuxt-bulma-slim.css')
+    const options = { cssTempPath, variablesPath: '/nope' }
 
     nuxtModule.call(context, options)
 
-    expect(existsSync(sassTempPath)).toBe(true)
+    expect(existsSync(cssTempPath)).toBe(true)
     expect(context.options.css).toHaveLength(1)
-    expect(context.options.css[0]).toBe(sassTempPath)
-
-    const lines = readFileSync(sassTempPath, 'utf8').split('\n')
-
-    expect(lines).toHaveLength(7) // each used feature
-    expect(lines[0].includes(bulmaPath)).toBe(true)
+    expect(context.options.css[0]).toBe(cssTempPath)
   })
 
   it('should include the variables file in the build', () => {
@@ -89,7 +84,7 @@ describe('Nuxt Module', () => {
         build: {}
       }
     }
-    const sassTempPath = join(tmpdir(), 'nuxt-bulma-slim.slim.sass')
+    const sassTempPath = join(tmpdir(), 'nuxt-bulma-slim.sass')
     const variablesPath = join(
       __dirname,
       '../example/assets/sass/variables.sass'
@@ -104,49 +99,37 @@ describe('Nuxt Module', () => {
 
     const lines = readFileSync(sassTempPath, 'utf8').split('\n')
 
-    expect(lines).toHaveLength(7) // each feature
-    expect(lines[1].includes(bulmaPath)).toBe(true)
+    expect(lines).toHaveLength(2)
+    expect(lines[0]).toBe(`@import "${variablesPath}"`)
+    expect(lines[1]).toBe(`@import "${join(bulmaPath, 'bulma.sass')}"`)
   })
 
-  it('should write full bulma css build if on development', () => {
+  it('should include the additional paths in the build', () => {
     const context = {
       options: {
         dev: true,
-        rootDir: join(__dirname, '../node_modules/lodash.trim'), // to read the package.json that doesnt have sass dependencies
+        rootDir: join(__dirname, '..'),
         srcDir: join(__dirname, '../example'),
         css: [],
         build: {}
       }
     }
-    const cssTempPath = join(tmpdir(), 'nuxt-bulma-slim.full.css')
-    const options = { cssTempPath, variablesPath: false }
+    const sassTempPath = join(tmpdir(), 'nuxt-bulma-slim.sass')
+    const additionalPaths = [
+      join(__dirname, '../example/assets/sass/additional.sass')
+    ]
+    const options = { sassTempPath, variablesPath: '/nope', additionalPaths }
 
     nuxtModule.call(context, options)
 
-    expect(existsSync(cssTempPath)).toBe(true)
+    expect(existsSync(sassTempPath)).toBe(true)
     expect(context.options.css).toHaveLength(1)
-    expect(context.options.css[0]).toBe(cssTempPath)
-    expect(existsSync(cssTempPath)).toBe(true)
-  })
+    expect(context.options.css[0]).toBe(sassTempPath)
 
-  it('should write slim bulma css build if on development', () => {
-    const context = {
-      options: {
-        dev: false,
-        rootDir: join(__dirname, '../node_modules/lodash.trim'), // to read the package.json that doesnt have sass dependencies
-        srcDir: join(__dirname, '../example'),
-        css: [],
-        build: {}
-      }
-    }
-    const cssTempPath = join(tmpdir(), 'nuxt-bulma-slim.slim.css')
-    const options = { cssTempPath, variablesPath: false }
+    const lines = readFileSync(sassTempPath, 'utf8').split('\n')
 
-    nuxtModule.call(context, options)
-
-    expect(existsSync(cssTempPath)).toBe(true)
-    expect(context.options.css).toHaveLength(1)
-    expect(context.options.css[0]).toBe(cssTempPath)
-    expect(existsSync(cssTempPath)).toBe(true)
+    expect(lines).toHaveLength(2)
+    expect(lines[0]).toBe(`@import "${join(bulmaPath, 'bulma.sass')}"`)
+    expect(lines[1]).toBe(`@import "${additionalPaths[0]}"`)
   })
 })
